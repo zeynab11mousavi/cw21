9db5246680095ef9d119550aab2df6376ab2deff
Zeynab Mousavi <zeynab11mousavi.com>
2022-08-18 16:05:50 +0430

    handle add

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,48 @@
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchTodos } from './redux/feature/todoSlice'
-import { useEffect } from 'react'
+import { fetchTodos, createTodos } from './redux/feature/todoSlice'
+import { useEffect, useState } from 'react'
 
 function App() {
   const { todos } = useSelector((state) => state.todos)
-
   const dispatch = useDispatch()
+  const [add, setAdd] = useState()
+
+  const handleAdd = () => {
+    if (add.length > 0) {
+      dispatch(createTodos({ text: add, done: false }))
+      dispatch(fetchTodos())
+      setAdd('')
+    }
+  }
+
   useEffect(() => {
     dispatch(fetchTodos())
   }, [])
+
   return (
-    <>
+    <div className="App">
       <h2>Todo List</h2>
       <div>
-        <input type="text" placeholder="add to do" />
-        <button>add</button>
+        <input
+          type="text"
+          onChange={(event) => setAdd(event.target.value)}
+          placeholder="add to do"
+        />
+        <button onClick={handleAdd}>add</button>
       </div>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            <span>{todo.text}</span> <button>Done</button>
-            <button>Remove</button>
+            <span>{todo.text}</span>
+            <div className="buttonHolder">
+              <button className="done">Done</button>
+              <button className="remove">Remove</button>
+            </div>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   )
 }
 
--- src/redux/feature/todoSlice.js
+++ src/redux/feature/todoSlice.js
@@ -14,13 +14,19 @@ const initialState = {
     error: ""
 }
 
-export const fetchTodos = createAsyncThunk("todos/fetchTodos", async () => {
+export const fetchTodos = createAsyncThunk("todos/fetchTodos", () => {
     return axios.get(BASE_URL).then(res => res.data)
+})
+
+export const createTodos = createAsyncThunk("todos/createTodos", (post) => {
+    return axios.post(BASE_URL, post).then(res => res.data)
 })
 const todoSlice = createSlice({
     name: "todos",
     initialState,
     extraReducers: {
+
+        // FETCH GET
         [fetchTodos.pending]: (state) => {
             state.loadings= true
         },
@@ -31,8 +37,24 @@ const todoSlice = createSlice({
         [fetchTodos.rejected]: (state) => {
             state.loadings= false;
             state.error = "some thing went wrong :( "
+        },
+
+        // FETCH POST 
+        [createTodos.pending]: (state) => {
+            state.loadings= true
+        },
+        [createTodos.fulfilled]: (state, action) => {
+            state.loadings= false;
+            // state.todos = action.payload;
+        },
+        [createTodos.rejected]: (state) => {
+            state.loadings= false;
+            state.error = "some thing went wrong :( "
         }
+
+
     }
+
 })
 
 
